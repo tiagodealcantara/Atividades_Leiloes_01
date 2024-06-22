55a07559254bcf773d96e4ba3f1d694134b1ab65
Tiago <tiagorodrigo_xavier@hotmail.com>
2024-06-21 21:30:37 -0300

    Atualizacao da funcionalidade do botao de consulta de vendas, filtro na tela de vendas dos produtos vendidos e criacao da tela de vendas

--- LeiloesTDSat/LeiloesTDSat/src/DAO/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/DAO/ProdutosDAO.java
@@ -4,12 +4,10 @@ package DAO;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
-
 import DAO.config.conectaDAO;
 import DTO.ProdutosDTO;
 import java.math.BigDecimal;
@@ -21,16 +19,15 @@ import java.util.ArrayList;
 import java.util.Set;
 import javax.swing.table.DefaultTableModel;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-            String sql = "insert into produtos (nome, valor, status) values (?, ?, ?);";
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        String sql = "insert into produtos (nome, valor, status) values (?, ?, ?);";
         try {
             conn = new conectaDAO().connectDB();
             prep = conn.prepareStatement(sql);
@@ -39,11 +36,11 @@ public class ProdutosDAO {
             try {
                 BigDecimal valor = new BigDecimal(produto.getValor());
                 prep.setBigDecimal(2, valor);
-                    int adicionado = prep.executeUpdate();
-                    if (adicionado > 0) {
-                        JOptionPane.showMessageDialog(null, "Serviço adicionado com sucesso!");
-                    }
-                
+                int adicionado = prep.executeUpdate();
+                if (adicionado > 0) {
+                    JOptionPane.showMessageDialog(null, "Serviço adicionado com sucesso!");
+                }
+
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "O valor deve ser um número decimal válido!");
             }
@@ -51,26 +48,23 @@ public class ProdutosDAO {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
-   
-        
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         String sql = "SELECT id, nome, valor, status FROM produtos;";
         try {
             conn = new conectaDAO().connectDB();
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
 
-
-            
             while (resultset.next()) {
                 ProdutosDTO model = new ProdutosDTO();
                 model.setId(resultset.getInt("id"));
                 model.setNome(resultset.getString("nome"));
                 model.setValor(resultset.getInt("valor"));
                 model.setStatus(resultset.getString("status"));
-                
+
                 listagem.add(model);
             }
 
@@ -79,9 +73,9 @@ public class ProdutosDAO {
         }
         return listagem;
     }
-    
-      public void venderProduto(int id){
-            String sql = "update produtos set status = 'Vendido' where id = ?;";
+
+    public void venderProduto(int id) {
+        String sql = "update produtos set status = 'Vendido' where id = ?;";
         try {
             conn = new conectaDAO().connectDB();
             prep = conn.prepareStatement(sql);
@@ -91,10 +85,30 @@ public class ProdutosDAO {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
-   
-        
-    }  
-    
-        
-}
 
+    }
+
+    public ArrayList<ProdutosDTO> produtosVendidos() {
+        String sql = "SELECT id, nome, valor, status FROM produtos WHERE status = 'Vendido';";
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO model = new ProdutosDTO();
+                model.setId(resultset.getInt("id"));
+                model.setNome(resultset.getString("nome"));
+                model.setValor(resultset.getInt("valor"));
+                model.setStatus(resultset.getString("status"));
+
+                listagem.add(model);
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return listagem;
+    }
+
+}
